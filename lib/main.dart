b90a5d44267047cfebf0441678de4abfc8e00da3
jean <jeancs.patricio@gmail.com>
2019-10-14 17:29:00 -0300

    Modificando tema

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'package:flutter_app/widgets/lista_transferencias.dart';
 
 void main() => runApp(
       MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme:  ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary
+          )
+        ),
         home: HomePage(),
       ),
     );
